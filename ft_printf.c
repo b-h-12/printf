243ad8e76c36ce7ae3baf800b13c6950d565ec71
b-h-12 <bilal.hamoum@hotmail.fr>
2025-04-05 14:45:44 +0200

    Started ft_printf implementation

---
+++ ft_printf.c
@@ -0,0 +1,63 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_printf.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: bhamoum <bhamoum@student.42.fr>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/04/05 14:11:32 by bhamoum           #+#    #+#             */
+/*   Updated: 2025/04/05 14:43:34 by bhamoum          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "ft_printf.h"
+
+int	put_format(char c, va_list args)
+{
+	int	i;
+	char	*str;
+	int	num;
+	
+	i = 0;
+	if (c == 'c')
+	{
+		ft_putchar_fd(va_arg(args, int), 1);
+		i++;
+	}
+	else if (c == 's')
+	{
+		str = ft_strdup(va_arg(args, char *));
+		ft_putstr_fd(str, 1);
+		i += ft_strlen(str);
+		free(str);
+	}
+}
+
+int	ft_printf(const char *format, ...)
+{
+	va_list	args;
+	int		i;
+
+	i = 0;
+	va_start(args, format);
+	while (format[i])
+	{
+		if (format[i] == '%')
+		{
+			i++;
+			if (format[i] == 'd')
+				ft_putnbr(va_arg(args, int));
+			else if (format[i] == 's')
+				ft_putstr(va_arg(args, char *));
+			else if (format[i] == 'c')
+				ft_putchar(va_arg(args, int));
+			else if (format[i] == '%')
+				ft_putchar('%');
+		}
+		else
+			ft_putchar(format[i]);
+		i++;
+	}
+	va_end(args);
+	return (i);
+}
