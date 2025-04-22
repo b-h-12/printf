d80be6c1bfbad17d9f65b9a386a8a45ea51823ae
b-h-12 <bilal.hamoum@hotmail.fr>
2025-04-22 16:50:27 +0200

    Refactor put_format_unsigned to improve unsigned integer handling and output

--- put_format_unsigned.c
+++ put_format_unsigned.c
@@ -6,23 +6,47 @@
 /*   By: bhamoum <bhamoum@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/14 15:45:24 by bhamoum           #+#    #+#             */
-/*   Updated: 2025/04/14 15:46:41 by bhamoum          ###   ########.fr       */
+/*   Updated: 2025/04/22 16:49:15 by bhamoum          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ft_printf.h"
 
+static void	put_nbr_unsigned_int(unsigned int n)
+{
+	if (n >= 10)
+	{
+		put_nbr_unsigned_int(n / 10);
+		put_nbr_unsigned_int(n % 10);
+	}
+	else
+	{
+		char	c;
+
+		c = n + '0';
+		write(1, &c, 1);
+	}
+}
+
 int	put_format_unsigned(va_list args)
 {
-	unsigned int	u_nb;
+	unsigned int	ui;
+	int	len;
+	char	c;
 
-	u_nb = va_arg(args, unsigned int);
-		if (u_nb > INT_MAX)
-		{
-			ft_putnbr_fd(u_nb / 10, 1);
-			ft_putnbr_fd(u_nb % 10, 1);
-		}
-		else
-			ft_putnbr_fd(u_nb, 1);
-	return ((u_nb / 10) + 1);
+	ui = va_arg(args, unsigned int);
+	c = '0';
+	if (ui == 0)
+	{
+		write(1, &c, 1);
+		return (1);
+	}
+	put_nbr_unsigned_int(ui);
+	len = 0;
+	while (ui != 0)
+	{
+		ui /= 10;
+		len++;
+	}
+	return (len);
 }
