c6bd8e2e1a62ac66d99a6fc4f5222d290fdb69dd
b-h-12 <bilal.hamoum@hotmail.fr>
2025-04-22 18:56:36 +0200

    Updated to handle NULL strings

--- put_format_str.c
+++ put_format_str.c
@@ -6,7 +6,7 @@
 /*   By: bhamoum <bhamoum@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/14 15:30:20 by bhamoum           #+#    #+#             */
-/*   Updated: 2025/04/14 15:34:34 by bhamoum          ###   ########.fr       */
+/*   Updated: 2025/04/22 18:05:12 by bhamoum          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,7 +17,13 @@ int	put_format_str(va_list args)
 	int		r;
 	char	*str;
 	
+	
 	str = ft_strdup(va_arg(args, char *));
+	if (!str)
+	{
+		write(1, "(null)", 6);
+		return (6);
+	}
 	ft_putstr_fd(str, 1);
 	r = ft_strlen(str);
 	free(str);
