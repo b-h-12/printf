883e332348b4f227e168fe3b43c0b603751cf5a8
b-h-12 <bilal.hamoum@hotmail.fr>
2025-04-22 16:50:18 +0200

    Fix length calculation in put_format_dec and handle zero case

--- put_format_dec.c
+++ put_format_dec.c
@@ -6,7 +6,7 @@
 /*   By: bhamoum <bhamoum@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/14 15:40:39 by bhamoum           #+#    #+#             */
-/*   Updated: 2025/04/22 15:58:06 by bhamoum          ###   ########.fr       */
+/*   Updated: 2025/04/22 16:21:36 by bhamoum          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,13 +16,21 @@ int	put_format_dec(va_list args)
 {
 	int	nb;
 	int	len;
+	int	negative;
 
 	nb = va_arg(args, int);
+	len = 0;
 	ft_putnbr_fd(nb, 1);
-	while (len > 0)
+	if (nb == 0)
+		return (1);
+	if (nb < 0)
+		negative = 1;
+	else
+		negative = 0;
+	while (nb != 0)
 	{
-		nb /= 16;
-		len ++;
+		nb /= 10;
+		len++;
 	}
-	return (len);
+	return (len + negative);
 }
